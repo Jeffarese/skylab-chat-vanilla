1241828243d62034f6d0f593dc23f3d490a334e0
Jeffarese <jeff@wallapop.com>
2017-02-06 18:39:26 +0100

    Finish send messages

--- js/controllers/chatController.js
+++ js/controllers/chatController.js
@@ -1,6 +1,18 @@
 angular.module("skylabChatControllers")
   .controller('chatController', function ($scope, messagesService, $firebaseObject, $firebaseArray) {
-    $scope.sendMessage = function(message) {
-      console.log(message);
+    var ref = firebase.database().ref();
+    var messagesRef = ref.child('messages');
+    var messages = [];
+    var messagesList = $firebaseArray(messagesRef);
+    console.log(messagesList);
+    $scope.getMessages = function() {
+      messagesList
+    };
+    $scope.sendMessage = function(message, user) {
+      console.log('Send');
+      messagesList.$add({
+        message: message,
+        user: user
+      });
     };
   });
